ef57a63464a9ee3192ae7b5472794b641b5fd379
Paul Filitchkin <filitchp@gmail.com>
2011-01-05 05:44:55 +0000

    Added licensing info

--- src/histLib.h
+++ src/histLib.h
@@ -1,5 +1,34 @@
-#ifndef HIST_LIB
-#define HIST_LIB
+//=================================================================================================
+// Copyright (c) 2011, Paul Filitchkin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, this list of
+//      conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright notice, this list of
+//      conditions and the following disclaimer in the documentation and/or other materials
+//      provided with the distribution.
+//
+//    * Neither the name of the <ORGANIZATION> nor the names of its contributors may be used
+//      to endorse or promote products derived from this software without specific prior written
+//      permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//=================================================================================================
+
+#ifndef _HIST_LIB
+#define _HIST_LIB
 
 #include <cv.h>
 
@@ -27,6 +56,7 @@ void DrawHistogramBGR(const cv::Mat& ImageBGR,  cv::Mat& ImageHist);
 
 void DrawHistogramGray(const IplImage* ImageBGR, IplImage* ImageHist);
 void DrawHistogramGray(const cv::Mat& ImageBGR,  cv::Mat& ImageHist);
+
 
 void DrawHistogram(CvHistogram* Hist,
                    unsigned HistSize,
@@ -88,4 +118,5 @@ void DrawHistBin(cv::Mat& HistLayer, double value, unsigned x,
              unsigned histBins       = HIST_BINS,
              unsigned histEdge       = HIST_EDGE,
              unsigned histHeight     = HIST_HEIGHT);
-#endif //end #ifndef HIST_LIB
+
+#endif //end #ifndef _HIST_LIB
