e5e8c8c4723f2d7ffdb9cf89ca34ee0bfb3e014c
Paul Filitchkin <filitchp@gmail.com>
2015-09-27 20:35:19 -0700

    Updated copyright

--- src/histLib.cpp
+++ src/histLib.cpp
@@ -1,5 +1,5 @@
 //=============================================================================
-// Copyright (c) 2012, Paul Filitchkin
+// Copyright (c) 2015, Paul Filitchkin
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
